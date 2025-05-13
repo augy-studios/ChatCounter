0228ae32f08bcd8ac1f41d3a9007a8fa15e2d103
Augy <ateoks@gmail.com>
2025-05-13 19:26:04 +0800

    add SHARD_COUNT and SHARD_IDS configuration to support sharding

--- config.py
+++ config.py
@@ -9,3 +9,5 @@ LOG_GUILD_ID = int(os.getenv("LOG_GUILD_ID")) if os.getenv("LOG_GUILD_ID") else 
 LOG_CHANNEL_ID = int(os.getenv("LOG_CHANNEL_ID")) if os.getenv("LOG_CHANNEL_ID") else None
 BOT_OWNER_ID = int(os.getenv("BOT_OWNER_ID")) if os.getenv("BOT_OWNER_ID") else None
 DISCORD_CLIENT_ID = int(os.getenv("DISCORD_CLIENT_ID")) if os.getenv("DISCORD_CLIENT_ID") else None
+SHARD_COUNT = int(os.getenv("SHARD_COUNT")) if os.getenv("SHARD_COUNT") else 1
+SHARD_IDS = [int(x) for x in os.getenv("SHARD_IDS").split(",")] if os.getenv("SHARD_IDS") else [0]
