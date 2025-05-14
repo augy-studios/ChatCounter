c40fbc2f00ff1779813ff287edb35d73262b573c
Augy <ateoks@gmail.com>
2025-05-14 23:16:37 +0800

    update help command to change support link

--- bot/commands/general.py
+++ bot/commands/general.py
@@ -63,8 +63,8 @@ class General(commands.Cog):
         embed = discord.Embed(title="Hello!", color=discord.Color.random())
         link_to_add_bot = f"https://discord.com/oauth2/authorize?client_id={self.bot.user.id}"
         embed.add_field(name="Add Me to Your Server", value=f"[Click here!]({link_to_add_bot})")
-        link_to_hello = "https://augystudios.com/"
-        embed.add_field(name="About Augy Studios", value=f"[Learn more about Augy Studios!]({link_to_hello})")
+        link_to_hello = "https://discord.gg/H4YYsgkYSa/"
+        embed.add_field(name="Bot Support", value=f"[Got questions? Join here!]({link_to_hello})")
         link_to_docs = "https://chatcounter.augystudios.com/"
         embed.add_field(name="How to Use", value=f"[View the documentation!]({link_to_docs})")
         embed.set_footer(text="Made with ❤️ by Augy Studios © 2025 All Rights Sniffed • https://augystudios.com/")
